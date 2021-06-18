29a68bba7a0a9c0be376125d9d1c7f710f1da4d6
romatimanov <romatimanov77@gmail.com>
2021-06-19 00:39:33 +0300

    changes

--- js/js.js
+++ js/js.js
@@ -418,6 +418,7 @@ function init() {
         .add(myPlacemarkWithContent);
 };
 
+
 var selector = document.querySelector("input[type='tel']");
 var im = new Inputmask("+7 (999) 999-99-99");
 
@@ -589,8 +590,33 @@ function modalOpen(curentModal) {
 function modalClose(modalActive, doUnlock = true) {
     if (unlock) {
         modalActive.classList.remove('open');
-        if (doUnlock) {
-            bodyunlock();
-        }
     }
+}
+
+// Отправка данных на сервер
+function send(event, php) {
+    console.log("Отправка запроса");
+    event.preventDefault ? event.preventDefault() : event.returnValue = false;
+    var req = new XMLHttpRequest();
+    req.open('POST', php, true);
+    req.onload = function() {
+        if (req.status >= 200 && req.status < 400) {
+            json = JSON.parse(this.response); // Ебанный internet explorer 11
+            console.log(json);
+
+            // ЗДЕСЬ УКАЗЫВАЕМ ДЕЙСТВИЯ В СЛУЧАЕ УСПЕХА ИЛИ НЕУДАЧИ
+            if (json.result == "success") {
+                // Если сообщение отправлено
+                alert("Сообщение отправлено");
+            } else {
+                // Если произошла ошибка
+                alert("Ошибка. Сообщение не отправлено");
+            }
+            // Если не удалось связаться с php файлом
+        } else { alert("Ошибка сервера. Номер: " + req.status); }
+    };
+
+    // Если не удалось отправить запрос. Стоит блок на хостинге
+    req.onerror = function() { alert("Ошибка отправки запроса"); };
+    req.send(new FormData(event.target));
 }
