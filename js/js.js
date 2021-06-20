2cbf9679c93d23ed4d5a88e03cc269cd22936244
romatimanov <romatimanov77@gmail.com>
2021-06-20 20:23:42 +0300

    changes js

--- js/js.js
+++ js/js.js
@@ -449,7 +449,20 @@ new JustValidate('.contacts__form', {
             required: 'Недопустимый формат!'
         }
     },
+    submitHandler: function(form, values, ajax) {
+
+        ajax({
+            url: 'https://just-validate-api.herokuapp.com/submit',
+            method: 'POST',
+            data: values,
+            async: true,
+            callback: function(response) {
+                console.log(response)
+            }
+        });
+    },
 });
+
 
 window.addEventListener('DOMContentLoaded', function() {
 
@@ -591,32 +604,4 @@ function modalClose(modalActive, doUnlock = true) {
     if (unlock) {
         modalActive.classList.remove('open');
     }
-}
-
-// Отправка данных на сервер
-function send(event, php) {
-    console.log("Отправка запроса");
-    event.preventDefault ? event.preventDefault() : event.returnValue = false;
-    var req = new XMLHttpRequest();
-    req.open('POST', php, true);
-    req.onload = function() {
-        if (req.status >= 200 && req.status < 400) {
-            json = JSON.parse(this.response); // Ебанный internet explorer 11
-            console.log(json);
-
-            // ЗДЕСЬ УКАЗЫВАЕМ ДЕЙСТВИЯ В СЛУЧАЕ УСПЕХА ИЛИ НЕУДАЧИ
-            if (json.result == "success") {
-                // Если сообщение отправлено
-                alert("Сообщение отправлено");
-            } else {
-                // Если произошла ошибка
-                alert("Ошибка. Сообщение не отправлено");
-            }
-            // Если не удалось связаться с php файлом
-        } else { alert("Ошибка сервера. Номер: " + req.status); }
-    };
-
-    // Если не удалось отправить запрос. Стоит блок на хостинге
-    req.onerror = function() { alert("Ошибка отправки запроса"); };
-    req.send(new FormData(event.target));
 }
